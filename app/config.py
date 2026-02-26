814a82ffba1c38749d32d672c2ddee64b2809843
ALAN ANTO <alan.anto.ai.2002@gmail.com>
2026-02-26 10:00:36 +0100

    Add VECTOR_DB_PATH to config.py

--- app/config.py
+++ app/config.py
@@ -6,4 +6,6 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 NEO4J_URL = os.getenv("NEO4J_URL")
 NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
+
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+VECTOR_DB_PATH = "./chroma_db"
